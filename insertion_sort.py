d4acd33859bf5602f5a66ae5e45ccc82f341bed5
Leo <leo@MacBook-Pro-de-Leo-3.local>
2021-01-18 17:33:36 +0100

    Update bulles, insertion and selection

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,20 +1,42 @@
 
-def tri_insertion(tab): 
+import sys
+import timeit
+nb_iti = 0
+
+def convertStringToNumber(array):
+    liste = []
+    for i in range(len(array)):
+        liste.append(float(array[i]))
+    return liste
+
+def insertion_sort(tab): 
     print(tab)
     
     for i in range(1, len(tab)): 
         k = float(tab[i]) 
-        print(k)
         j = i-1
-        print(j)
         while j >= 0 and k < tab[j] : 
                 tab[j + 1] = tab[j] 
                 j -= 1
         tab[j + 1] = k
 
 
-tab = [200, 12, -18, 30, 2, 74, 6.3, 70]
-tri_insertion(tab) 
-print ("Le tableau trié est:")
-for i in range(len(tab)): 
-    print ("% d" % float(tab[i]))
+tab = convertStringToNumber(sys.argv[1].split(";"))
+sorted_tab = []
+
+insertion_sort(tab)
+
+for i in range(len(tab)):
+    sorted_tab.append(tab[i])
+
+print("Serie")
+print(convertStringToNumber(sys.argv[1].split(";")))
+print("Resultat")
+print(sorted_tab)
+print("Nb de comparaison")
+print(nb_iti)
+print("Nb d iteration")
+print(nb_iti)
+print("Temps (sec)")
+execution_time = timeit.timeit('insertion_sort(tab)', 'from __main__ import insertion_sort, tab', number=1)
+print(execution_time)
--- selection_sort.py
+++ selection_sort.py
@@ -1,8 +1,16 @@
-def tri_selection(tab):
+import sys
+import timeit
+nb_iti = 0
+
+def convertStringToNumber(array):
+    liste = []
+    for i in range(len(array)):
+        liste.append(float(array[i]))
+    return liste
+
+def selection_sort(tab):
 
    for i in range(len(tab)):
-
-      # Trouver le min
        min = i
 
        for j in range(i+1, len(tab)):
@@ -15,11 +23,22 @@ def tri_selection(tab):
 
    return tab
 
-# Programme principale pour tester le code ci-dessus
-tab = [99, 20, 15, 32, 2, 74, 63, 72]
+tab = convertStringToNumber(sys.argv[1].split(";"))
+sorted_tab = []
+
+selection_sort(tab)
  
-tri_selection(tab)
- 
-print ("Le tableau trié est:")
 for i in range(len(tab)):
-    print ("%d" %tab[i])
+    sorted_tab.append(tab[i])
+
+print("Serie")
+print(convertStringToNumber(sys.argv[1].split(";")))
+print("Resultat")
+print(sorted_tab)
+print("Nb de comparaison")
+print(nb_iti)
+print("Nb d iteration")
+print(nb_iti)
+print("Temps (sec)")
+execution_time = timeit.timeit('selection_sort(tab)', 'from __main__ import selection_sort, tab', number=1)
+print(execution_time)
